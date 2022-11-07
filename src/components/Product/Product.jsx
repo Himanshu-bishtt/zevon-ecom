2ea2d11f2a3da5b84f7a869880bb94c2cc3034a8
Himanshu Bisht <himanshubisht313@gmail.com>
2022-11-07 18:08:34 +0530

    added link to products list so that product detail page can load

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './Product.module.scss';
 
-const Product = ({ title, price, image, category, rate, count }) => {
+const Product = ({ id, title, price, image, category, rate, count }) => {
   return (
-    <div className={classes.product}>
-      <img
-        src={image}
-        className={`${classes.image} skeleton-loader`}
-        alt={title}
-      />
-      <div className={classes.content}>
-        <div className={classes.head}>
-          <h3>{category}</h3>
-          <h2 title={title}>{title}</h2>
-        </div>
-        <p className={classes.price}>${price}</p>
+    <Link to={id.toString()}>
+      <div className={classes.product}>
+        <img
+          src={image}
+          className={`${classes.image} skeleton-loader`}
+          alt={title}
+        />
+        <div className={classes.content}>
+          <div className={classes.head}>
+            <h3>{category}</h3>
+            <h2 title={title}>{title}</h2>
+          </div>
+          <p className={classes.price}>${price}</p>
 
-        <div className={classes.rating}>
-          <p className={classes.stars}>{rate} ⭐</p>
-          <p className={classes.count}>({count})</p>
+          <div className={classes.rating}>
+            <p className={classes.stars}>{rate} ⭐</p>
+            <p className={classes.count}>({count})</p>
+          </div>
         </div>
+        {rate >= 4 && <p className={classes.ratingBadge}>Top Rated</p>}
       </div>
-      {rate >= 4 && <p className={classes.ratingBadge}>Top Rated</p>}
-    </div>
+    </Link>
   );
 };
 

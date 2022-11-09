7ca84b862d7c0f40ec42304848d7078103ee6ab5
Himanshu Bisht <himanshubisht313@gmail.com>
2022-11-09 22:51:29 +0530

    wishlist icon added on product item

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { WishlistIcon } from '../../icons';
 import classes from './Product.module.scss';
 
 const Product = ({
@@ -13,28 +15,31 @@ const Product = ({
   customURL = id.toString(),
 }) => {
   return (
-    <Link to={customURL}>
-      <div className={classes.product}>
-        <img
-          src={image}
-          className={`${classes.image} skeleton-loader`}
-          alt={title}
-        />
-        <div className={classes.content}>
-          <div className={classes.head}>
-            <h3>{category}</h3>
+    <div className={classes.product}>
+      <img
+        src={image}
+        className={`${classes.image} skeleton-loader`}
+        alt={title}
+      />
+      <div className={classes.content}>
+        <div className={classes.head}>
+          <h3>{category}</h3>
+          <Link to={customURL}>
             <h2 title={title}>{title}</h2>
-          </div>
-          <h2 className={classes.price}>${price}</h2>
-
-          <div className={classes.rating}>
-            <p className={classes.stars}>{rate} ⭐</p>
-            <p className={classes.count}>({count})</p>
-          </div>
+          </Link>
         </div>
-        {rate >= 4 && <p className={classes.ratingBadge}>Top Rated</p>}
+        <h2 className={classes.price}>${price}</h2>
+
+        <div className={classes.rating}>
+          <p className={classes.stars}>{rate} ⭐</p>
+          <p className={classes.count}>({count})</p>
+        </div>
       </div>
-    </Link>
+      {rate >= 4 && <p className={classes.ratingBadge}>Top Rated</p>}
+      <button className={classes.wishlist} title="Wishlist this item">
+        <WishlistIcon />
+      </button>
+    </div>
   );
 };
 

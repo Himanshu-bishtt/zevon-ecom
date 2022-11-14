7479976e51a8e48e768934e5a193a531ef447f50
Himanshu Bisht <himanshubisht313@gmail.com>
2022-11-14 23:08:46 +0530

    search functionality working

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { Icons } from '../../icons';
+import React, { useEffect, useState } from 'react';
+import { getProducts } from '../../api/api';
+import SearchItem from './SearchItem';
 
+import { Icons } from '../../icons';
 import classes from './Search.module.scss';
 
 const Search = () => {
+  const [products, setProducts] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await getProducts();
+      setProducts(response);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const searchInputChangeHandler = event => {
+    const items = products.filter(item =>
+      item.title.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+
+    setFilteredItems(items);
+  };
+
   return (
     //prettier-ignore
     <>
@@ -11,8 +37,13 @@ const Search = () => {
       <svg className={classes.icon}>
         <use href={`${Icons}#icon-search`} />
       </svg>
-      <input type="text" placeholder="Enter item to search..." className={classes.input}/>
-      <div className={classes.suggestions}></div>
+      <input type="text" placeholder="Enter item to search..." className={classes.input} onChange={searchInputChangeHandler}/>
+      <ul className={classes.suggestions}>
+        {filteredItems.slice(0, 5).map((item, index) => (
+          <SearchItem key={index} id={item.id} title={item.title} image={item.image} category={item.category}/>
+        ))}
+      </ul>
+
     </form>
     </>
   );
--- src/components/Search/SearchItem.jsx
+++ src/components/Search/SearchItem.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import classes from './SearchItem.module.css';
+import classes from './SearchItem.module.scss';
 
 const SearchItem = ({ id, title, image, category, onClick }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const imageLoaded = event => {
+    console.log(event);
+    setIsLoading(false);
+  };
   return (
     <li
       className={classes['suggestions-item']}
       title={title}
       onClick={() => onClick(id)}
     >
-      <img src={image} alt={title} className="skeleton-loader" />
-      <div style={{ width: '80%' }}>
-        <h2 className={classes.title}>{title}</h2>
-        <h3 className={classes.category}>{category}</h3>
+      <img
+        src={image}
+        alt={title}
+        className={isLoading ? 'skeleton-loader' : ''}
+        onLoad={imageLoaded}
+      />
+      <div
+        style={{ width: '80%' }}
+        className={isLoading ? 'skeleton-loader' : ''}
+      >
+        <h2>{title}</h2>
+        <h3>{category}</h3>
       </div>
     </li>
   );

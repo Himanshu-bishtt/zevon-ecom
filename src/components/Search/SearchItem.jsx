a7eaf275039a0fedf572090d280ea623abfbf8fe
Himanshu Bisht <himanshubisht313@gmail.com>
2022-11-15 12:28:20 +0530

    link added to search suggestion products

--- src/components/Search/SearchItem.jsx
+++ src/components/Search/SearchItem.jsx
@@ -1,33 +1,38 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { toggleSearchBar } from '../../store/ui-slice';
 
 import classes from './SearchItem.module.scss';
 
-const SearchItem = ({ id, title, image, category, onClick }) => {
+const SearchItem = ({ id, title, image, category }) => {
+  const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(true);
   const imageLoaded = event => {
     console.log(event);
     setIsLoading(false);
   };
   return (
-    <li
-      className={classes['suggestions-item']}
-      title={title}
-      onClick={() => onClick(id)}
+    <Link
+      to={`products/${category.replaceAll(' ', '-')}/${id}`}
+      onClick={() => dispatch(toggleSearchBar())}
     >
-      <img
-        src={image}
-        alt={title}
-        className={isLoading ? 'skeleton-loader' : ''}
-        onLoad={imageLoaded}
-      />
-      <div
-        style={{ width: '80%' }}
-        className={isLoading ? 'skeleton-loader' : ''}
-      >
-        <h2>{title}</h2>
-        <h3>{category}</h3>
-      </div>
-    </li>
+      <li className={classes['suggestions-item']} title={title}>
+        <img
+          src={image}
+          alt={title}
+          className={isLoading ? 'skeleton-loader' : ''}
+          onLoad={imageLoaded}
+        />
+        <div
+          style={{ width: '80%' }}
+          className={isLoading ? 'skeleton-loader' : ''}
+        >
+          <h2>{!isLoading && title}</h2>
+          <h3>{!isLoading && category}</h3>
+        </div>
+      </li>
+    </Link>
   );
 };
 
